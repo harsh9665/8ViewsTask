64c15413a3c87e0f6939aa1d49ce651e5b09d1a6
Revella Harshith <harsh.revella@gmail.com>
2026-04-03 02:46:51 +0530

    fixe QuoteSection

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -1,25 +1,34 @@
 import styles from '@/styles/QuoteSection.module.css';
+import Image from 'next/image';
 
 export default function QuoteSection() {
   return (
-    <section className={styles.quoteSection} >
-      <div className={styles.inner  + ' container'}>
-        {/* Left quote marks */}
-        <div className={styles.quoteMark}>&ldquo;&rdquo;</div>
+    <section className={styles.quoteSection}>
 
-        {/* Main quote text */}
+      {/* Decorative Quote */}
+      <Image
+        src="/images/quotes.png"
+        alt="Quotes"
+        width={83}
+        height={77}
+        className={styles.quoteMark}
+      />
+
+      <div className={`${styles.inner} container`}>
         <div className={styles.quoteContent}>
           <p className={styles.quoteText}>
             We work at the intersection of architectural design, construction
-            science, and ecological research. Where dreams takes shape, spaces
+            science, and ecological research. Where dreams take shape, spaces
             come alive
           </p>
+
           <div className={styles.brand}>
             <span className={styles.brandDot}></span>
-            <span className={styles.brandName}>E- INFRA</span>
+            <span className={styles.brandName}>E-INFRA</span>
           </div>
         </div>
       </div>
+
     </section>
-  );
+  )
 }
